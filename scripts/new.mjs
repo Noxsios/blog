ef93a7bf5b5230546fa2170a407d12848f795dc2
razzle <harry@razzle.cloud>
2023-12-18 12:07:29 -0600

    `zarf dev deploy` (#11)
    * init article
    
    Signed-off-by: razzle <harry@razzle.cloud>
    
    * more progress
    
    Signed-off-by: razzle <harry@razzle.cloud>
    
    * almost r4r
    
    Signed-off-by: razzle <harry@razzle.cloud>
    
    * init tape
    
    Signed-off-by: razzle <harry@razzle.cloud>
    
    * fix tape link
    
    Signed-off-by: razzle <harry@razzle.cloud>
    
    * edits
    
    Signed-off-by: razzle <harry@razzle.cloud>
    
    * edits
    
    Signed-off-by: razzle <harry@razzle.cloud>
    
    * vale
    
    Signed-off-by: razzle <harry@razzle.cloud>
    
    * some AI feedback
    
    Signed-off-by: razzle <harry@razzle.cloud>
    
    * some AI feedback
    
    Signed-off-by: razzle <harry@razzle.cloud>
    
    * fix links
    
    Signed-off-by: razzle <harry@razzle.cloud>
    
    * release the kraken
    
    Signed-off-by: razzle <harry@razzle.cloud>
    
    ---------
    
    Signed-off-by: razzle <harry@razzle.cloud>

--- scripts/new.mjs
+++ scripts/new.mjs
@@ -21,9 +21,14 @@ draft = true
 
 [taxonomies]
 tags = ${JSON.stringify(tags, null, 0)}
+
+[extra]
+meta = [
+    {property = "og:image", content = "https://og.razzle.cloud/og?title=${encodeURI(title)}"},
+]
 +++
 
-> tldr;
+> **TL;DR**
 
 <!-- more -->
 
